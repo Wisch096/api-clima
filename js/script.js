bb4ed7fbf3f7c0e459010d67bac82f76d7fd4f1f
Wisch096 <wisch.pg@outlook.com>
2022-11-05 14:47:53 -0300

    começando-api-clima

---
+++ js/script.js
@@ -0,0 +1,6 @@
+const apiKey = "cebc585a79541557982e4f0785620567";
+const apiCountryURL = "https://countryflagsapi.com/png/"
+
+//mapear
+const cityInput = document.querySelector("#city-input");
+const searchBtn = document.querySelector("#search");
